84129462c6d56ef08abfc348fe317e0225472517
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-10-09 10:00:01 +0600

    init commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
+import Counter from './src/component/Counter';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Counter color={'lightblue'} size={16} />
+        <Counter color={'skyblue'} size={32} />
+        <Counter color={'steelblue'} size={80} />
+        <Counter color={'darkblue'} size={140} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ src/component/Counter.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+export default class Counter extends Component {
+  state = {count: 0};
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({count: this.state.count + 1});
+    }, 1000);
+  }
+
+  render() {
+    const {count} = this.state;
+    const {color, size} = this.props;
+
+    return <Text style={{color, fontSize: size}}>{count}</Text>;
+  }
+}
